a4670b4e5bcba3de2556342beb724b3fa999e968
kelvin78226 <rosendokelvin8@gmail.com>
2023-02-19 14:24:47 -0300

    Definindo Perguntas

--- ADD.py
+++ ADD.py
@@ -4,3 +4,10 @@
 #   mostrar sites que ajudem o usuario em suas duvidas
 #através de perguntas
 
+nome = input("qual é o seu nome")
+
+sono = int(input("você tem dormido quantas horas por noite ?, {:.2f} ").format(nome))
+alimentação = int(input("como você tem se alimetado ?, "))
+carga_horaria_de_trabalho = int(input("quantas horas você tem trabalhado ? "))
+descanço = input("você tem arranjado tempo para seu lazer ?")
+rotina = input("você tem uma rotina pré-escrita para o seu dia ?")
