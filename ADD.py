c7a341538f4fbd66e8d228944374e0bd8bee4a7c
kelvin78226 <rosendokelvin8@gmail.com>
2023-02-19 14:12:59 -0300

    objetivos do codigo

---
+++ ADD.py
@@ -0,0 +1,6 @@
+#objetivos do codigo:
+#    ajudar a melhorar o desempenho em qualquer coisa
+#    interagir pelo input com o usuario 
+#   mostrar sites que ajudem o usuario em suas duvidas
+#através de perguntas
+
